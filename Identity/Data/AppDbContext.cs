56f5d419f88054b575be9be70b45b1b6b17bc8e8
Lenovo <Lenovo@DESKTOP-OP7NDBS>
2024-05-08 17:18:19 +0430

    Add project files.

---
+++ Identity/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Identity.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Identity.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<product> products { get; set; }
+    }
+
+}
+
---
+++ Identity/Models/product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models
+{
+    public class product
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int price { get; set; }
+
+
+    }
+}
